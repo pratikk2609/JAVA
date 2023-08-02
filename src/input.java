9319f263dc5f7d55dd69d926274ed43aec8c88e8
Pratik Pawar <pratikpawar2609@gmail.com>
2023-08-02 21:19:59 +0530

    inputs

--- src/input.java
+++ src/input.java
@@ -3,13 +3,17 @@ public class input {
     public static void main(String[] args) {
         System.out.println("taking inputs from user");
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter num 1");
-        int a = sc.nextInt();
-        System.out.println("enter num 2");
-        int b = sc.nextInt();
-        int sum = a+b;
-        System.out.println("sum of two nums is");
-        System.out.println(sum);
+//        System.out.println("enter num 1");
+//        int a = sc.nextInt();
+//        System.out.println("enter num 2");
+//        int b = sc.nextInt();
+//        int sum = a+b;
+//        System.out.println("sum of two nums is");
+//        System.out.println(sum);
+//        boolean b1 =sc.hasNextInt();
+//        System.out.println(b1);
+        String str = sc.next();
+        System.out.println(str);
 
     }
 }
