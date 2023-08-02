54e6bf9bb9e837d616bff38dc3773469a5bb93aa
Pratik Pawar <pratikpawar2609@gmail.com>
2023-08-02 23:02:01 +0530

    question

---
+++ src/q2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        System.out.println("enter ur number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
