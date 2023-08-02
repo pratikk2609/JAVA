2a2f94a3bcbe98748f8b36eddac085ac942beeeb
Pratik Pawar <pratikpawar2609@gmail.com>
2023-08-02 21:09:44 +0530

    input

---
+++ src/input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args) {
+        System.out.println("taking inputs from user");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter num 1");
+        int a = sc.nextInt();
+        System.out.println("enter num 2");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("sum of two nums is");
+        System.out.println(sum);
+
+    }
+}
