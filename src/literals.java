81f2e5c28c1f4ee6e963c9b33d76e7453c2e8dbf
Pratik Pawar <pratikpawar2609@gmail.com>
2023-08-02 20:56:57 +0530

    Initial commit

---
+++ src/literals.java
@@ -0,0 +1,13 @@
+public class literals {
+    public static void main(String[] args)
+    {
+        byte age = 18;
+        int age2 = 50;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        System.out.println(age);
+        String str = "Pratik";
+        System.out.println(str);
+    }
+}
